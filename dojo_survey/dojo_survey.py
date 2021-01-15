d8db36dfe48b533d7cafa85586639c2faf0833e0
RowdyOnline <rowdy14@gmail.com>
2021-01-14 21:42:53 -0800

    Updated to include session

--- dojo_survey/dojo_survey.py
+++ dojo_survey/dojo_survey.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session
 app = Flask(__name__)
+app.config.from_pyfile('config.py')
 
 @app.route('/')
 def index():
@@ -7,13 +8,17 @@ def index():
 
 @app.route('/ninja', methods=['POST'])
 def create_user():
-    name=request.form['name']
-    location=request.form['location']
-    language=request.form['language']
-    course=request.form['course']
-    comment=request.form['comment']
-    OS=request.form.getlist('OS')
-    return render_template('show.html', userName=name, userLocation=location, userLanguage=language, userCourse=course, userComment=comment, userOS=OS)
+    session['name']=request.form['name']
+    session['location']=request.form['location']
+    session['language']=request.form['language']
+    session['course']=request.form['course']
+    session['comment']=request.form['comment']
+    session['OS']=request.form.getlist('OS')
+    return redirect('/show_user')
+
+@app.route('/show_user')
+def show_user():
+    return render_template('show.html', userName=session['name'], userLocation=session['location'], userLanguage=session['language'], userCourse=session['course'], userComment=session['comment'], userOS=session['OS'])
 
 
 if __name__ == '__main__':
